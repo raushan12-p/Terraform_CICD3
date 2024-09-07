2608047f0bf367ac04aa9ec772e06874b1f05553
raushan12-p <raushan1054@gmail.com>
2024-09-07 21:45:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-00db8dadb36c9815e"
+    ami = "ami-0ae8f15ae66fe8cda"
     instance_type = "t2.nano"
     tags = {
       Name = "dev-ec3"
